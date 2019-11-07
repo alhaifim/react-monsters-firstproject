ad6a2379c4801dbdf14bab239ecc3ec44ea6d598
Mohammed Al-Haifi <alhaifim@outlook.com>
2019-11-06 23:27:09 -0700

    Updates

---
+++ precache-manifest.52146ac595f1a9a3e4d0210f046c9bfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ade4308a9b6589618e39456ecb7c86",
+    "url": "/react-monsters-firstproject/index.html"
+  },
+  {
+    "revision": "93cadb3987dab71e3b54",
+    "url": "/react-monsters-firstproject/static/css/main.e0a6e980.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/react-monsters-firstproject/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "93cadb3987dab71e3b54",
+    "url": "/react-monsters-firstproject/static/js/main.ead9b312.chunk.js"
+  },
+  {
+    "revision": "2a46f5bc0e9a9e35e37b",
+    "url": "/react-monsters-firstproject/static/js/runtime-main.92a132d6.js"
+  }
+]);
